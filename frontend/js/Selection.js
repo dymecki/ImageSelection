2d302be1c010ca0f1a91a59e46c8a7fd1b9f8235
Michał Dymecki <michal@dymecki.com>
2016-09-13 15:34:24 +0200

    Added Util object and Util.hasClass() method.

--- frontend/js/Selection.js
+++ frontend/js/Selection.js
@@ -169,5 +169,28 @@
         }, false);
     };
 
+
+
+    /* ======================================================= */
+
+    var Util = function() {};
+
+    Util.prototype.hasClass = function(singleClassName, wholeClassName) {
+        var classes = wholeClassName.split(' ');
+
+        for (var i = 0; i < classes.length; i++) {
+            if (classes[i] == singleClassName) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+
+
+    /* ======================================================= */
+
+
     var selection = new Selection();
 })();
